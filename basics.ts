aaaba8175dbefc0e1a48e31424ccb1afb3bab16c
Sathishkumar Raja <satraja@WKMZT0645EE6.global.publicisgroupe.net>
2022-11-09 08:12:21 +0530

    types example

--- basics.ts
+++ basics.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add1(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('Incorrect input!');
   // }
@@ -13,7 +13,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 let number1: number;
 number1 = 5;
 const number2 = 2.8;
-const printResult = true;
+// const printResult = true;
 let resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult, resultPhrase);
+add1(number1, number2, true, resultPhrase);

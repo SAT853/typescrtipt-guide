bee0bcb4d260c1141e07c17a2d75144566bc215a
Sathishkumar Raja <satraja@WKMZT0645EE6.global.publicisgroupe.net>
2022-11-10 11:17:16 +0530

    interfaces and classes example

--- class&Interfaces/src/app.ts
+++ class&Interfaces/src/app.ts
@@ -1,49 +1,76 @@
 
 class Department {
-    name: string;
-    private employess: string[] = [];
+    protected employess: string[] = []; 
+    constructor(private id: string, private name: string) { 
+    };
+    addEmployess(e: string) {
+        this.employess.push(e)
+    };
+    printEmployess() {
+        console.log(this.employess)
+    };
+}
 
-    constructor(n: string){
-        this.name = n
+let accounting = new Department("d1", "accounting");
+accounting.addEmployess("Sathish");
+console.log(accounting)
+
+
+class ITDepartment extends Department{
+    constructor(id: string,  private admin: string[],){
+        super(id, "IT");
+        this.admin = admin
     }
-private addEmployess(e: string){
-    this.employess.push(e)
-}
-
-
-printEmployess(e: string){
-    this.addEmployess(e)
-    console.log(this.employess)
-}
-
-    describe(this: Department){
-        console.log("Department: ",this.name)
+    printAdmin(){
+        console.log(this.admin)
+    }
+    addEmployess(e: string): void {
+        if(e === "raja") return;
+        this.employess.push(e);
     }
 }
 
-let accounting = new Department("accounting");
+let it = new ITDepartment("d2", ["Jhon"]);
+it.addEmployess("Sathish")
+console.log(it);
 
-
-// accounting.addEmployess("Sathish");
-// accounting.addEmployess("Raja");
-accounting.printEmployess("Sathish")
-
-// accounting.describe()
-
-
-// let accountingCopy = {name: "management", describe: accounting.describe}
-// accountingCopy.describe()
-
-
-// Shorthand Initialization 
-
+// Getter and Setter;
 class Person{
-    constructor(private id: string, private name: string){}
-    printNameAndID(){
-        console.log(`${this.id} ${this.name}`)
+    private name: string;
+    get personName(){
+        return this.name
+    }
+    set personName(n: string){
+        this.name = n;
+    }
+    constructor(name: string){
+        this.name = name
     }
 }
+let p1 = new Person("Sathish");
+p1.personName = "updated..."
+console.log(p1.personName);
 
-let p1 = new Person("1", "Sathish");
+// Abstruct type
 
-p1.printNameAndID()
+abstract class Department1 {
+    static fiscalYear = 2020;
+    // private readonly id: string;
+    // private name: string;
+    protected employees: string[] = [];
+  
+    constructor(protected readonly id: string, public name: string) {
+      // this.id = id;
+      // this.name = n;
+      // console.log(Department.fiscalYear);
+    }
+    abstract describe(this: Department): void;
+
+  }
+  
+
+  class IT extends Department1{
+    describe(this: Department): void {
+    
+    }   
+  }
